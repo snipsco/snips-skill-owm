0ea50b2945442dae73536bdf4a9a6b19ed40b2e8
Michael Fester <michael.fester@gmail.com>
2017-07-20 15:33:22 +0200

    Update skill

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='snips_skill_weather',
+    name='snips_skill_weather_owm',
     version='0.1.0',
-    description='',
+    description='OpenWeatherMap weather skill for Snips',
     author=['Michael Fester'],
     author_email=['michael.fester@snips.ai'],
-    url='',
+    url='https://www.snips.ai',
     download_url='',
     license='MIT',
     install_requires=['paho-mqtt','requests==2.6.0','pyyaml'],
     tests_require=[],
     packages=[
-        'snips_skill_weather'
+        'snips_skill_weather_owm'
     ]
 )
