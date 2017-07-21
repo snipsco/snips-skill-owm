37f13c276cc589b28782b9839e9a47037f300a3c
Michael Fester <michael.fester@gmail.com>
2017-07-21 12:41:48 +0200

    Update tests

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='snipsowm',
-    version='0.1.1',
+    version='0.1.3',
     description='OpenWeatherMap weather skill for Snips',
     author='Michael Fester',
     author_email='michael.fester@gmail.com',
-    url='https://github.com/snipsco/snips-skill-weather-owm',
+    url='https://github.com/snipsco/snips-skill-owm',
     download_url='',
     license='MIT',
-    install_requires=['paho-mqtt', 'requests==2.6.0', 'pyyaml'],
+    install_requires=['requests==2.6.0'],
     test_suite="tests",
     keywords = ['snips'],
     packages=[
--- tests/test_skill.py
+++ tests/test_skill.py
@@ -2,11 +2,11 @@ from unittest import TestCase
 
 from snipsowm.snipsowm import SnipsOWM
 
-class TestSkills(TestCase):
+class TestSkill(TestCase):
 
     def setUp(self):
         self.skill = SnipsOWM("e3deb9f92803fce990166d1af2f3d0fc", "Paris,fr")
 
     def test_skill(self):
-    	response = self.skill.execute()
+        response = self.skill.execute()
         self.assertNotEqual(len(response), 0)
