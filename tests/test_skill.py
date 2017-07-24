4c13f1497a4b3569f0573bb4c18e1a62fa0cab39
Michael Fester <michael.fester@gmail.com>
2017-07-24 12:52:21 +0200

    Update tests

--- tests/test_skill.py
+++ tests/test_skill.py
@@ -8,6 +8,6 @@ class TestSkill(TestCase):
     def setUp(self):
         self.skill = SnipsOWM("e3deb9f92803fce990166d1af2f3d0fc", "Paris,fr")
 
-    def test_skill(self):
-        response = self.skill.execute()
-        self.assertNotEqual(len(response), 0)
+    def test_get_weather(self):
+        response = self.skill.get_weather("Paris,fr")
+        self.assertEqual(len(response), 2)
