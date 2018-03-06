92738304a22d502c449f9480e2fd26d9c041e4b9
Anthony Reinette <anthonyray@users.noreply.github.com>
2018-03-06 16:08:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='snipsowm',
-    version='0.2.0',
+    version='0.2.1',
     description='OpenWeatherMap weather skill for Snips',
     author='Michael Fester',
     author_email='michael.fester@gmail.com',
