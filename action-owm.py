6158cdd31072f90f4779e00072aa07a60f054f71
Bjay Watanabe Kamwa <bjay.kamwa-watanabe@snips.ai>
2018-10-01 14:25:11 +0200

    Add locale to config.ini

--- action-owm.py
+++ action-owm.py
@@ -183,8 +183,8 @@ if __name__ == "__main__":
         print "No API key in config.ini, you must setup an OpenWeatherMap API key for this skill to work"
     elif len(config.get("secret").get("api_key")) == 0:
         print "No API key in config.ini, you must setup an OpenWeatherMap API key for this skill to work"
-
-    skill_locale = config["global"].get("locale", "en_US")
+    
+    skill_locale = config.get("global",{"locale":"en_US"}).get("locale", "en_US")
 
     skill = SnipsOWM(config["secret"]["api_key"],
                      config["secret"]["default_location"],locale=skill_locale)
